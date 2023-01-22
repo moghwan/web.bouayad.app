d44aaf62dca52a0bf3553864074f26019e484749
moghwan <chaoui.mar2@gmail.com>
2023-01-22 18:56:14 +0100

    cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
   ],
   theme: {
     extend: {},
