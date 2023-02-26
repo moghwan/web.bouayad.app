9284f8d5b06f9f32a2d373367e9e2efe5335adc8
moghwan <chaoui.mar2@gmail.com>
2023-02-26 18:06:12 +0100

    initial setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { fileURLToPath, URL } from "node:url";
+import { VitePWA } from 'vite-plugin-pwa';
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        }
+      }),
+  ],
   define: {
     'process.env': process.env
   },
