328c23053c44f01c42163c13fad0550eadc5c7c7
moghwan <chaoui.mar2@gmail.com>
2023-03-04 02:48:52 +0100

    centered loader

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <TheDay v-if="data" :data="data" @parent-refreshtheday="refreshTheDay" :selectedCityId="selectedCityId"/>
-  <spinner class="flex justify-centclassNamescreen items-center" v-if="!data"/>
+  <spinner class="flex justify-center h-screen items-center" v-if="!data"/>
 </template>
 
 <script setup>
