bb6631f5af2de0ede88579f1d13bacf90ebdcb29
moghwan <chaoui.mar2@gmail.com>
2023-03-05 16:38:08 +0100

    adding result as data attribute

--- src/components/partials/salateElement.vue
+++ src/components/partials/salateElement.vue
@@ -1,8 +1,8 @@
 <template>
-  <td :class="salate.class">
+  <td :class="salate.class" v-bind:data-result="salate.result">
     {{ salate.time }} 
   </td>
-  <td class="border-r" :class="salate.class">
+  <td class="border-r" :class="salate.class" v-bind:data-result="salate.result">
     {{ salate.name_ar }}
   </td>
 </template>
