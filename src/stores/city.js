624fd899036dc67ff477ad408c96d0606a25dcb3
moghwan <chaoui.mar2@gmail.com>
2023-03-10 19:35:02 +0100

    removing counter store

--- src/stores/city.js
+++ src/stores/city.js
@@ -1,16 +1,6 @@
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import { defineStore } from "pinia";
 import { useLocalStorage } from "@vueuse/core"
-
-export const useCounterStore = defineStore("counter", () => {
-  const count = ref(0);
-  const doubleCount = computed(() => count.value * 2);
-  function increment() {
-    count.value++;
-  }
-
-  return { count, doubleCount, increment };
-});
 
 export const useCityStore = defineStore("selectedCity", () => {
   const cityId = ref(useLocalStorage('count', 1));
