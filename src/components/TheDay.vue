0a837f5e6ff93d1578b0f3edfe0ce83a7fe3457a
moghwan <chaoui.mar2@gmail.com>
2023-03-11 21:59:02 +0100

    min with to max

--- src/components/TheDay.vue
+++ src/components/TheDay.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center h-screen items-center">
-    <div class="block rounded-lg shadow-lg bg-white min-w-fit text-center sm:w-96 w-9/12">
+    <div class="block rounded-lg shadow-lg bg-white max-w-sm text-center sm:w-96 w-9/12">
       <!--main layout-->
       <div class="flex flex-col">
         <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
