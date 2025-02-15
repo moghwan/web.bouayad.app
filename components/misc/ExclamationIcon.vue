4f085628119bbcead6a80985bb60fab87d49ecbf
moghwan <chaoui.mar2@gmail.com>
2025-02-15 20:22:04 +0100

    add exclamation mark icon

---
+++ components/misc/ExclamationIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <svg aria-hidden="true" class="w-8 h-8 text-red-600" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="12" cy="12" r="10" fill="currentColor"/>
+      <path d="M12 8V12" stroke="white" stroke-width="2" stroke-linecap="round"/>
+      <circle cx="12" cy="16" r="1.5" fill="white"/>
+    </svg>
+  </div>
+</template>
